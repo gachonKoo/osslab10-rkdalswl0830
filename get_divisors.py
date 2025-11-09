e6686802c5a8d6ad007584cfb0970732ec0b3e6c
Minji Kang <minjik0830@gachon.ac.kr>
2025-11-09 23:28:50 +0900

    fix: autograder format

--- get_divisors.py
+++ get_divisors.py
@@ -1,10 +1,6 @@
-# Lab 10: Get divisors
+# Lab10 autograding version
 
-n = int(input("Enter a number: "))
-
-divisors = []
+n = int(input())
 for i in range(1, n + 1):
     if n % i == 0:
-        divisors.append(i)
-
-print("Divisors of", n, "are:", divisors)
+        print(i)
